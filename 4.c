8311a9be3fadc169cb02e31347684c4485147058
HZ-Cyq <1369509439@qq.com>
2018-01-03 08:49:40 +0800

    强制转换类型规避类型系统

--- 4.c
+++ 4.c
@@ -6,29 +6,30 @@
  ************************************************************************/
 
 #include<stdio.h>
-typedef char *byte_pointer;
+#include<string.h>
+typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer a,int length) {
-    printf("length: %d\n",length);
-    for (int i = 0; i < length; i ++) {
-        printf("%.2x\t",a[i]);
-    }
+    int i;    
+    for (i = 0; i < length; i ++)
+        printf(" %.2x",a[i]);
     printf("\n");
 }
 void show_int(int a) {
     show_bytes((byte_pointer)(&a),sizeof(int));
 }
 void show_float(float a) {
-    show_bytes((byte_pointer)(&a),sizeof(float));
-    printf("showfloat is over\n");
+    show_bytes((byte_pointer) &a,sizeof(float));
 }
 void show_pointer(void *a) {
-    show_bytes((byte_pointer)&a,sizeof(void *));//代表指向NULL的指针
+    show_bytes((byte_pointer) &a,sizeof(void *));//代表指向NULL的指针
 }
 int main() {
     show_int(123);
-    show_float(1.23);//迷之输出 char类型的数据竟然可以输出8位，前面6位是f 具体输出是这样的：ffffffa4	70	ffffff9d	3f	 
+    show_float(1.23);
+//终于找到bug了，原来是定义的char指针，应该是unsigned类型，这样的话避免了是负数的时候会输出奇怪的值，
+//也许是因为超过边界了，所以系统改成8位输出，就象1.c中溢出了还会输出正确结果
     show_float(23.098);
-    void *a;
-    show_pointer(a);
+    char *s = "ABCDEF";
+    show_bytes((byte_pointer)s,strlen(s));
     return 0;
 }
